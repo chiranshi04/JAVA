71b710be9a4bd19fad052c76be138b45b872e93c
chiranshi04 <chiranshiyadav919@gmail.com>
2026-08-30 15:04:58 +0530

    Create RectangleModelDriver.java

---
+++ WEEK_4/Rectangle/RectangleModelDriver.java
@@ -0,0 +1,9 @@
+public class RectangleModelDriver 
+{
+    public static void main(String[] args)
+    {
+        RectangleModel first = new RectangleModel();
+        RectangleModel second = new RectangleModel(5);
+        RectangleModel third = new RectangleModel(5,8);
+    }
+}   
