9b065c126a18d7d5cf4d411b94965de1956debbb
chiranshi04 <chiranshiyadav919@gmail.com>
2026-09-20 18:09:49 +0530

    Add InvalidStudentDataException class

---
+++ Week 7/src/college/model/InvalidStudentDataException.java
@@ -0,0 +1,11 @@
+package college.model;
+/**
+ * InvalidStudentDataException
+ */
+public class InvalidStudentDataException extends Exception{
+public InvalidStudentDataException(String message)
+{
+    super(message);
+}
+    
+}
